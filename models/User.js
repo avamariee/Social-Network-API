fea2b70bc1bb521988e6d60cee3d1a6606e0e4ee
Ava Marie Bullock <ava.m.bullock@gmail.com>
2021-05-22 15:18:37 -0600

    created User model

--- models/User.js
+++ models/User.js
@@ -2,4 +2,45 @@
 const { Schema, model, Types } = required('mongoose');
 
 
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: 'Username is Required.',
+        trim: true
+    },
+    email: {
+        type: String,
+        required: 'Email is Required.',
+        unique: true,
+        match: [/.+@.+\..+/]
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+    // this is very possibly incorrect, will test later
+})
+
+const User = model('User', UserSchema)
+
 module.exports = User
