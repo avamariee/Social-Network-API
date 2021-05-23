67fd720894e068bff8d06c6a95140bf01ccde14f
Ava Marie Bullock <ava.m.bullock@gmail.com>
2021-05-23 15:21:52 -0600

    created and got a thought by ID

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -4,21 +4,41 @@ const User = require('../models/User')
 const thoughtController = {
     // get all thoughts
 
-    getThoughts(req, res){
+    getThoughts(req, res) {
         Thought.find({})
-        .select('-__v')
-        .sort({ _id: -1 })
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log("There was an error. " + err);
-            res.status(400).json(err)
-        });
+            .select('-__v')
+            .sort({ _id: -1 })
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => {
+                console.log("There was an error. " + err);
+                res.status(400).json(err)
+            });
     },
 
     // get a single thought by its _id
 
-    // post to create a new thought, push to associated user's thoughts array field
+    getThoughtsById({ params }, res) {
+        Thought.findOne({ _id: params.id })
+            .select('-__v')
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with this ID.' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
 
+    // post to create a new thought, push to associated user's thoughts array field
+    createThought({ body }, res) {
+        Thought.create(body)
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => res.status(400).json(err));
+    },
     // put to update a thought by its _id
 
     // delete to remove a thought by its _id
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -2,11 +2,19 @@
 const router = require('express').Router();
 
 const {
-    getThoughts
+    getThoughts, getThoughtsById, createThought
 } = require('../../controllers/thought-controller')
 
+// GET and POST requests for localhost:3001/api/thoughts
 router
 .route('/')
 .get(getThoughts)
+.post(createThought);
+
+// GET ONE, PUT & DELETE for localhost:3001/api/thoughts/:id
+
+router
+.route('/:id')
+.get(getThoughtsById)
 
 module.exports = router;
