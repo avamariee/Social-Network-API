f6a98d57ca68b26d4e90a1241b530506bafa03cd
Ava Marie Bullock <ava.m.bullock@gmail.com>
2021-05-23 14:54:37 -0600

    successfully created a USER and got that USER by ID, had to comment out thoughts model because have not finished code for that yet

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -12,10 +12,10 @@ const userController = {
 
     getAllUser(req, res) {
         User.find({})
-            .populate({
-                path: 'thoughts',
-                select: '-__v'
-            })
+            // .populate({
+            //     path: 'thoughts',
+            //     select: '-__v'
+            // })
             .select('-__v')
             .sort({ _id: -1 })
             .then(dbUserData => res.json(dbUserData))
@@ -24,13 +24,40 @@ const userController = {
                 res.status(400).json(err)
             });
     },
+
+    // GET a single user by its _id and populated thought and friend data
+
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+            // .populate({
+            //     path: 'thoughts',
+            //     select: '-__v'
+            // })
+            .select('-__v')
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'There is no User with this ID.' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log('There was an error. FIND ONE USER ' + err);
+                res.status(400).json(err);
+            });
+    },
+
+    // POST a new user
+
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err));
+    },
 }
 
 
-// GET a single user by its _id and populated thought and friend data
 
-
-// POST a new user
 
 
 // PUT - update a user by its _id
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -3,11 +3,20 @@ const router = require('express').Router();
 
 
 const {
-    getAllUser
+    getAllUser, getUserById, createUser
 } = require('../../controllers/user-controller')
 
+// GET and POST requests for localhost:3001/api/users
 router
 .route('/')
-.get(getAllUser);
+.get(getAllUser)
+.post(createUser);
+
+
+// GET ONE, PUT & DELETE for localhost:3001/api/users/:id
+
+router
+.route('/:id')
+.get(getUserById)
 
 module.exports = router;
